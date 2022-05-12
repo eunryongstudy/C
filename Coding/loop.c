584d9d18ba21657e8dc62e5d028e868aefd99205
eunryong <studyeunryong@gmail.com>
2022-05-12 22:20:03 +0900

    *계단 역순

--- Coding/loop.c
+++ Coding/loop.c
@@ -65,8 +65,20 @@ int main(void) {
 			printf("  %d x %d = %d\n", i, j, i * j);
 		}
 	}*/
-	for (int i = 0; i < 5;i++) {
+	/*for (int i = 0; i < 5;i++) {
 		for (int j = 0; j <=i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}*/
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 5 - 1; j++)
+		{
+			printf(" ");
+		}
+			for (int k = 0; k <= i; k++) 
+		{
 			printf("*");
 		}
 		printf("\n");
