cc454c6bcc649509d8322a01d7eba296dbcd50e4
eunryong <studyeunryong@gmail.com>
2022-05-12 17:40:02 +0900

    for 문

--- Coding/loop.c
+++ Coding/loop.c
@@ -28,5 +28,15 @@ int main(void) {
 	//printf("Hello World %d\n", i++); //4
 	//printf("Hello World %d\n", i++); //5
 
+	//반복문
+	//for,while,do while
+
+	//for (선언; 조건; 증감)
+	for (int i = 1;i <=10; i++)
+	{
+		printf("Hello World %d\n", i);
+	}
+
+
 	return 0;
 }
