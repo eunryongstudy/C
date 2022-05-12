e58e729ad5af59f3e88ac8aedd67ba238b000382
eunryong <studyeunryong@gmail.com>
2022-05-12 22:14:50 +0900

    *계단

--- Coding/loop.c
+++ Coding/loop.c
@@ -59,11 +59,17 @@ int main(void) {
 			printf("	두 번째 반복문 : %d\n", j);
 		}
 	}*/
-	for (int i = 2; i <= 9; i++) {
+	/*for (int i = 2; i <= 9; i++) {
 		printf("%d단 계산 \n", i);
 		for (int j = 1; j <= 9; j++) {
 			printf("  %d x %d = %d\n", i, j, i * j);
 		}
+	}*/
+	for (int i = 0; i < 5;i++) {
+		for (int j = 0; j <=i; j++) {
+			printf("*");
+		}
+		printf("\n");
 	}
 
 	return 0;
