a604e199bf1bd5956abb84fc6c779cadb012adf9
eunryong <studyeunryong@gmail.com>
2022-05-12 22:30:10 +0900

    피라미드 쌓기

--- Coding/loop.c
+++ Coding/loop.c
@@ -71,13 +71,27 @@ int main(void) {
 		}
 		printf("\n");
 	}*/
-	for (int i = 0; i < 5; i++)
+	/*for (int i = 0; i < 5; i++)
 	{
 		for (int j = i; j < 5 - 1; j++)
 		{
 			printf(" ");
 		}
 			for (int k = 0; k <= i; k++) 
+		{
+			printf("*");
+		}
+		printf("\n");
+	}*/
+	int floor;
+	printf("¸î ÃþÀ¸·Î ½×°Ú´À³Ä? : ");
+	scanf_s("%d", &floor);
+	for (int i = 0; i < floor; i++) {
+		for (int j = i; j < floor - 1; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < i * 2 +1; k++)
 		{
 			printf("*");
 		}
