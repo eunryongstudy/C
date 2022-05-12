87542fc3d0a45a159b64bf8e2a284c35f7806eff
eunryong <studyeunryong@gmail.com>
2022-05-12 09:18:39 +0900

    변수 , 상수

--- Coding/printfscanf.c
+++ Coding/printfscanf.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 int main(void)
 {
-	printf("Hello World\n");
+	//정수형 변수
+	/*int age = 28;
+	printf("%d\n", age);
+	age = 14;
+	printf("%d\n", age);*/
+	//실수형 변수
+	/*float f = 68.5f;
+	printf("%.3f\n", f);
+	double d = 55.53;
+	printf("%.2lf\n", d);*/
+	//상수
+	/*const int YEAR = 1995;
+	printf("태어난 년도 : %d\n", YEAR);*/
 	return 0;
 }
