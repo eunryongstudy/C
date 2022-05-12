5db1e7a635d9dc6babe6abe8c8d4ec84ca5e7172
eunryong <studyeunryong@gmail.com>
2022-05-12 17:31:31 +0900

    ++ 2

--- Coding/loop.c
+++ Coding/loop.c
@@ -15,10 +15,18 @@ int main(void) {
 	a++;
 	printf("a= %d\n", a);*/
 
+	/*
 	int b = 20;
 	printf("b = %d\n", ++b);
 	printf("b = %d\n", b++);
-	printf("b = %d\n", b);
+	printf("b = %d\n", b);*/
+
+	//int i = 1;
+	//printf("Hello World %d\n", i++); //1
+	//printf("Hello World %d\n", i++); //2
+	//printf("Hello World %d\n", i++); //3
+	//printf("Hello World %d\n", i++); //4
+	//printf("Hello World %d\n", i++); //5
 
 	return 0;
 }
