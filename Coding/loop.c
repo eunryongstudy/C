c17297fb027df4bf0503e4be85b9e9af7dba64df
eunryong <studyeunryong@gmail.com>
2022-05-12 22:07:50 +0900

    구구단

--- Coding/loop.c
+++ Coding/loop.c
@@ -50,13 +50,19 @@ int main(void) {
 		printf("Hello World %d\n", i++);
 	} while (i <= 10);*/
 	//2중 반복문
-	for (int i = 1; i <= 4; i++)
+	/*for (int i = 1; i <= 4; i++)
 	{
 		printf("첫 번째 반복문 : %d\n", i);
 
 		for (int j = 1; j <= 5; j++)
 		{
 			printf("	두 번째 반복문 : %d\n", j);
+		}
+	}*/
+	for (int i = 2; i <= 9; i++) {
+		printf("%d단 계산 \n", i);
+		for (int j = 1; j <= 9; j++) {
+			printf("  %d x %d = %d\n", i, j, i * j);
 		}
 	}
 
