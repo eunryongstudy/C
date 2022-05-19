b664606501a38e3d9625e01c73bf808fd93a9230
eunryong <studyeunryong@gmail.com>
2022-05-19 22:33:58 +0900

    typedef

--- Coding/struct.c
+++ Coding/struct.c
@@ -9,6 +9,15 @@ struct GameInfo {
 	struct GameInfo* friendGame;
 };
 
+typedef struct GameInfomatin {
+	char* name;
+	int year;
+	int price;
+	char* company;
+
+	struct GameInfo* friendGame;
+} GAME_INFO; 
+
 int main(void)
 {
 	
@@ -63,5 +72,26 @@ int main(void)
 	printf("  가격 : %d\n", gameInfo1.friendGame->price);
 	printf("  제작사 : %s\n", gameInfo1.friendGame->company);
 
+
+	//typedef
+	int i = 1;
+	typedef int 정수;
+	typedef float 실수;
+	정수 정수변수 = 3;
+	실수 실수변수 = 3.23f;
+	printf("\n\n정수변수 = %d 실수변수 = %.2f\n\n", 정수변수, 실수변수);
+
+	typedef struct GameInfo 게임정보;
+	게임정보 game1;
+	game1.name = "한글게임";
+	game1.year = 1997;
+	game1.price = 600;
+	game1.company = "한글회사";
+
+	GAME_INFO game2;
+	game2.name = "영어게임";
+	game2.year = 1994;
+	game2.price = 700;
+	game2.company = "영어회사";
 	return 0;
 }
